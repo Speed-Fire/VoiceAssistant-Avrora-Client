bf89cef5ad336be5966b0483fb6403975133ef8d
Vladislav Sidorovich <vlad44812@gmail.com>
2025-08-01 23:05:46 +0200

    Dixing files structure

---
+++ Core/VoiceAssistant.Client.Abstractions/IKeyPhraseListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceAssistant.Client.Abstractions
+{
+	public interface IKeyPhraseListener : IDisposable
+	{
+		Task ListenAsync(CancellationToken stoppingToken);
+	}
+}
---
+++ Core/VoiceAssistant.Client.Abstractions/IServerCommunicator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceAssistant.Client.Abstractions
+{
+	public interface IServerCommunicator
+	{
+		Task<Stream> SendCommandAsync(byte[] audio, CancellationToken stoppingToken);
+	}
+}
